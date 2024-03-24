107694a1c84aa8bfaeb976c989476ae6c04768b2
Joone Hur <joone.hur@gmail.com>
2024-03-24 15:39:44 -0700

    Add functionality to gather tags from posts
    Create a function to gather unique tags from posts for future use in
    tagging articles and improving search functionality.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const config = require("./config");
 const addHomePage = require("./homepage");
 const addListPage = require("./list");
 
+function gatherTags(posts) {
+  const tags = new Set();
+  posts.forEach(post => {
+    const tagArray = post.attributes.tags.split(",");
+    tagArray.forEach(tag => tags.add(tag));
+  });
+  return Array.from(tags);
+}
+
+// Read all markdown articles from content/posts and sort them by date
 const posts = fs
   .readdirSync(config.dev.postsdir)
   .map(post => postMethods.readArticle(post))
@@ -15,11 +25,17 @@ const posts = fs
 
 if (!fs.existsSync(config.dev.outdir)) fs.mkdirSync(config.dev.outdir);
 
+// Create posts in docs/posts
 postMethods.createPosts(posts);
 
 addHomePage(posts);
 addListPage(posts);
 
+// Create tags page
+const tags = gatherTags(posts);
+console.log(tags);
+
 // Create about page
 const about = aboutMethod.readAbout(config.dev.about);
 aboutMethod.createAbout(about);
+
