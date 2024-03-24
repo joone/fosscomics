5a59103c1845e70db38ff455d3e7b4828b237ac7
Joone Hur <joone.hur@gmail.com>
2024-03-23 21:03:30 -0700

    Update static file directory to 'docs'
    Changed the static file directory from 'public' to 'docs' to better
    reflect the content being served.

--- webserver/index.js
+++ webserver/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static("docs"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
