042312d4a8d0842106b548623930ab40aaa11418
Joone Hur <joone.hur@gmail.com>
2024-03-24 17:35:35 -0700

    Fix heading tag in listpage function
    Changed the heading tag in the listpage function from <p> to <h1> for
    correct semantic structure. Added missing closing tag for <ul>.

--- src/list.js
+++ src/list.js
@@ -23,9 +23,8 @@ const listpage = posts => `
                   <a href="/tags">Tags</a>
                 </nav>
             </header>
-              <h1 class="page-title">All articles</p>
+              <h1 class="page-title">All articles</h1>
               <ul class="posts">
-
                 ${posts
                   .map(
                     post => `<li class="post">
@@ -38,6 +37,7 @@ const listpage = posts => `
                     </li>`
                   )
                   .join("")}
+              </ul>
             <footer>
               <div style="display:flex">
                 <a class="soc" href="https://github.com/joone/fosscomics" rel="me" title="GitHub"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a>
