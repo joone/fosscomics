cb59fb073d7fe1ee62bdd58b8768363fde991acc
Joone Hur <joone.hur@gmail.com>
2024-03-27 21:26:50 -0700

    Add examples

---
+++ examples/img.mjs
@@ -0,0 +1,45 @@
+// Create reference instance
+import { marked } from 'marked';
+
+// Override function
+const renderer = {
+  image(href, title, text) {
+    let size = null;
+
+    // Check if the title contains a size specification
+    if (title && title.includes('size:')) {
+        const sizeMatch = title.match(/size:(\d+%)/);
+        if (sizeMatch && sizeMatch[1]) {
+            size = sizeMatch[1];
+            // Remove the size specification from the title
+            title = title.replace(/size:\d+%/g, '').trim();
+        }
+    }
+
+    // Construct the image tag with optional size and title
+    let imageTag = `<img src="${href}" alt="${text}"`;
+    if (size) {
+        imageTag += ` style="width: ${size};"`;
+    }
+    imageTag += '>';
+
+    return `
+    <div style="text-align: center;">
+       <figure style="text-align: center;">
+         ${imageTag}
+         ${title ? `<figcaption>${title}</figcaption>` : ""}
+       </figure>
+     </div>
+`;
+  },
+
+};
+
+marked.use({ renderer });
+
+// Run marked
+console.log(marked.parse('![Figures of John Mauchly](images/image3.png "John Mauchly size:100%")'));
+
+console.log(marked.parse('![Figures of John Mauchly](images/image3.png "John Mauchly")'));
+
+console.log(marked.parse('![Figures of John Mauchly](images/image3.png)'));
---
+++ examples/renderer.mjs
@@ -0,0 +1,31 @@
+// Create reference instance
+import { marked } from 'marked';
+
+// Override function
+const renderer = {
+  heading(text, level) {
+    const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+    return `
+            <h${level}>
+              <a name="${escapedText}" class="anchor" href="#${escapedText}">
+                <span class="header-link"></span>
+              </a>
+              ${text}
+            </h${level}>`;
+  },
+  image(href, title, text) {
+    let titleAttribute = title !="null" ? `title="${title}" ` : "";
+    return `
+      <div style="text-align: center;">
+        <img src="${href}" alt="${text}" ${titleAttribute}/>
+      </div>`;
+  },
+
+};
+
+marked.use({ renderer });
+
+// Run marked
+///console.log(marked.parse('# heading+'));
+console.log(marked.parse('![alt_text](images/image13.png "image_tooltip")'));
