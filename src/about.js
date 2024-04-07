91857af48bdbe09e82f423d901d464e058b3463c
Joone Hur <joone.hur@gmail.com>
2024-04-06 23:49:13 -0700

    Update dynamic content generation in about page

--- src/about.js
+++ src/about.js
@@ -5,24 +5,6 @@ const common = require("./mod/common");
 const config = require("./mod/config");
 const marked = require("./mod/marked");
 
-const posthtml = (post) => {
-  const postTemplate = fs.readFileSync(
-    "./themes/archie/layouts/page.html",
-    "utf-8",
-  );
-
-  const jsString = "return () => " + `\`${postTemplate}\`;`;
-  const funcPost = new Function("post, config, common", jsString);
-  const result = funcPost(post, config, common)();
-  const array = result.split("\n");
-  for (let i = 0; i < array.length; i++) {
-    if (i !== 0) array[i] = `${array[i]}`;
-  }
-
-  const postHTML = array.join("\n");
-  return postHTML;
-};
-
 const readAbout = () => {
   const aboutPath = config.dev.about;
   const post = fs.readFileSync(aboutPath, "utf8");
@@ -36,9 +18,11 @@ const createAboutPage = () => {
   const about = readAbout();
   if (!fs.existsSync(`${config.dev.outdir}/about/`))
     fs.mkdirSync(`${config.dev.outdir}/about/`);
+
+  const data = { post: about };
   fs.writeFile(
     `${config.dev.outdir}/about/index.html`,
-    posthtml(about),
+    common.generateHTML("./themes/archie/layouts/page.html", data),
     (e) => {
       if (e) throw e;
       console.log(`${about.path}/about/index.html was created successfully`);
