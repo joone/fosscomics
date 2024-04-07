cce221b667f56aa43b1d03d8e46da8b1992fdf54
Joone Hur <joone.hur@gmail.com>
2024-04-06 23:04:11 -0700

    Refactor about.js to generate about.html using page.html template file

--- src/about.js
+++ src/about.js
@@ -5,65 +5,23 @@ const common = require("./mod/common");
 const config = require("./mod/config");
 const marked = require("./mod/marked");
 
-const posthtml = (post) => `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="../styles/fonts.css">
-        <link rel="stylesheet" href="../styles/main.css">
-        <link rel="icon" type="image/png" href="../images/favicon.png">
-        <!-- Google tag (gtag.js) -->
-        ${config.googleAnalyticsID ? common.googleAnalytics(config.googleAnalyticsID) : ""}
-        <title>${config.blogName}: ${post.attributes.title}</title>
-        <meta name="description" content="${config.blogDescription}" />
-        ${common.openGraph(
-          "website",
-          config.blogName,
-          `${config.blogsite}/about.html`,
-          `${config.blogName}: ${post.attributes.title}`,
-          config.blogDescription,
-          config.image,
-        )}
+const posthtml = (post) => {
+  const postTemplate = fs.readFileSync(
+    "./themes/archie/layouts/page.html",
+    "utf-8",
+  );
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="${config.blogsite}" />
-        <meta name="twitter:title" content="${config.blogName}: ${post.attributes.title}" />
-        <meta name="twitter:description" content="${config.blogDescription}" />
-        <meta name="twitter:image" content="${post.attributes.image}" />
-    </head>
-    <body>
-        <div class="content">
-            <header>
-                <div class="main">
-                  ${config.blogName}
-                </div>
-                <nav class="site-navigation">
-                  <a href="/">Home</a>
-                  <a href="/all_posts">All posts</a>
-                  About
-                  <a href="/tags">Tags</a>
-                </nav>
-            </header>
-            <main>
-              <article class="content">
-                  <div class="title">
-                    <h1 class="title">${post.attributes.title}</h1>
-                  </div>
-                  <section class="body">
-                    ${post.body}
-                  </section>
-              </article>
-            </main>
+  const jsString = "return () => " + `\`${postTemplate}\`;`;
+  const funcPost = new Function("post, config, common", jsString);
+  const result = funcPost(post, config, common)();
+  const array = result.split("\n");
+  for (let i = 0; i < array.length; i++) {
+    if (i !== 0) array[i] = `${array[i]}`;
+  }
 
-            <footer>
-              ${common.footer()}
-            </footer>
-        </div>
-    </body>
-</html>
-`;
+  const postHTML = array.join("\n");
+  return postHTML;
+};
 
 const readAbout = () => {
   const aboutPath = config.dev.about;
