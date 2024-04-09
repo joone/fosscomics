950fb33906cb0254d903d024927d64714e03ce90
Joone Hur <joone.hur@gmail.com>
2024-04-09 14:46:30 -0700

    Move the custom renderer of markdown to marked.js

--- src/mod/marked.js
+++ src/mod/marked.js
@@ -16,4 +16,80 @@ marked.setOptions({
   xhtml: false,
 });
 
+// Override function
+const renderer = {
+  image(href, title, text) {
+    let size = null;
+    // Check if the title contains a size specification
+    if (title && title.includes("size:")) {
+      const sizeMatch = title.match(/size:(\d+%)/);
+      if (sizeMatch && sizeMatch[1]) {
+        size = sizeMatch[1];
+        // Remove the size specification from the title
+        title = title.replace(/size:\d+%/g, "").trim();
+      }
+    }
+    let align = "center";
+    if (title && title.includes("align:")) {
+      // align: left, right, center
+      const alignMatch = title.match(/align:(left|right|center)/);
+      if (alignMatch && alignMatch[1]) {
+        align = alignMatch[1];
+        // Remove the alignment specification from the title
+        title = title.replace(/align:(left|right|center)/g, "").trim();
+      }
+    }
+
+    // Construct the image tag with optional size and title
+    let imageTag = `<img src="${href}" alt="${text}"`;
+    if (size) {
+      imageTag += ` style="width: ${size};"`;
+    }
+    imageTag += ">";
+
+    return `
+        <div style="text-align: ${align};">
+          <figure style="text-align: ${align};">
+            ${imageTag}
+            ${title ? `<figcaption>${title}</figcaption>` : ""}
+          </figure>
+        </div>
+      `;
+  },
+  link(href, title, text) {
+    let align = null;
+    if (title && title.includes("align:")) {
+      // align: left, right, center
+      const alignMatch = title.match(/align:(left|right|center)/);
+      if (alignMatch && alignMatch[1]) {
+        align = alignMatch[1];
+        // Remove the alignment specification from the title
+        title = title.replace(/align:(left|right|center)/g, "").trim();
+      }
+    }
+    // if align is not null, add div with text-align style
+    if (align) {
+      return `
+          <div style="text-align: ${align};">
+            <a href="${href} "${title ? `title="${title}"` : ""}>${text}</a>
+          </div>`;
+    } else {
+      return `<a href="${href} "${title ? `title="${title}"` : ""}>${text}</a>`;
+    }
+  },
+  blockquote(quote) {
+    return `<div class="blockquote-container"><blockquote>${quote}</blockquote></div>`;
+  },
+  paragraph(text) {
+    // remove <p> surrounding the image
+    if (text.includes('<figure style="text-align: center;">')) {
+      return text;
+    } else {
+      return `<p>${text}</p>`;
+    }
+  },
+};
+
+marked.use({ renderer });
+
 module.exports = marked;
