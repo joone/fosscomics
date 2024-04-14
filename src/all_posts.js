f564e9a9d180ae8c98154121a4b90b4d0d437e5e
Joone Hur <joone.hur@gmail.com>
2024-04-13 21:34:34 -0700

    Update fs.writeFile to fs.writeFileSync for synchronous operation
    Changed fs.writeFile to fs.writeFileSync for synchronous file writing operation
    to improve performance and ensure data integrity.
    
    Generated by gpt-3.5-turbo

--- src/all_posts.js
+++ src/all_posts.js
@@ -16,7 +16,7 @@ module.exports = class AllPostsPage extends PageBase {
     this.title = `${this.config.blogName}: ${data.pageTitle}`;
     this.description = "All posts";
     this.imageURL = this.config.image;
-    fs.writeFile(
+    fs.writeFileSync(
       `${this.config.dev.outdir}/all_posts/index.html`,
       this.generateHTML("./themes/archie/layouts/all_posts.html", data),
       (e) => {
