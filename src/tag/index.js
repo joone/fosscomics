f89c49bc57de521dbeec75469453a0834a6f87cf
Joone Hur <joone.hur@gmail.com>
2024-04-13 22:00:58 -0700

    Refactor generateContent method in TagList class
    Remove unnecessary parameter pageBase in generateContent method to
    simplify the code and improve readability.
    
    Generated by gpt-3.5-turbo

--- src/tag/index.js
+++ src/tag/index.js
@@ -29,7 +29,7 @@ module.exports = class TagList extends PageBase {
     return tags;
   }
 
-  generateContent(articles, pageBase) {
+  generateContent(articles) {
     //tagArray = Array.from(tags.keys());
 
     const tagMap = this.gatherTags(articles);
