4a83cddcf81fb907c4a21f883afc64e14b218086
Joone Hur <joone@outlook.com>
2026-06-29 01:52:24 -0700

    Migrate to fossbook npm package
    Replace custom src/ implementation with fossbook.
    - Remove src/ (home.js, posts.js, all_posts.js, tag/, mod/)
    - Add fossbook as the only runtime dependency
    - Add fossbook.config.js with site settings and comments config
    - Update scripts: build → fossbook build, start → fossbook serve
    - Remove themes/ (fossbook bundles the archie theme)

---
+++ fossbook.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  blogName: "F/OSS Comics",
+  authorName: "Joone Hur",
+  authorDescription: "Open Source Developer and Comic Artist",
+  authorWebsite: "https://fosscomics.com",
+  blogDescription:
+    "F/OSS Comics is a comic series dedicated to Free and Open Source Software. The comics aim to educate and entertain readers about the world of free and open source software.",
+  blogsite: "https://fosscomics.com",
+  githubCNAME: "fosscomics.com",
+  googleAnalyticsID: "G-M0CWE9F5HJ",
+  authorTwitter: "@joone",
+  siteTwitter: "@fosscomics",
+  githubRepository: "https://github.com/joone/fosscomics",
+  image:
+    "https://fosscomics.com/8.%20UNIX%20and%20C%20Language/images/feature.png",
+  theme: "archie",
+  comments: {
+    provider: "utterances",
+    repo: "joone/fosscomics",
+    issueTerm: "pathname",
+    theme: "github-light",
+  },
+};
