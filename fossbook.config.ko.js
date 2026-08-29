0011dbb8ff79a0daedc713d55849c68f193b243f
Joone Hur <joone@outlook.com>
2026-08-29 12:52:45 -0700

    Localize Korean navigation labels

--- fossbook.config.ko.js
+++ fossbook.config.ko.js
@@ -1,4 +1,9 @@
 module.exports = {
+  blogName: "F/OSS 코믹스",
   blogDescription:
     "F/OSS Comics는 자유 및 오픈 소스 소프트웨어를 다루는 만화 시리즈입니다. 컴퓨터와 소프트웨어의 역사를 통해 독자들이 자유 및 오픈 소스 소프트웨어의 세계를 이해하고 직접 참여할 수 있도록 돕고자 합니다.",
+  homeLabel: "대문",
+  allPostsLabel: "모든 글",
+  aboutLabel: "소개",
+  tagsLabel: "태그",
 };
