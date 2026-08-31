f5d5f1918cdd28375db0125a7d0abf5da7c6fd8c
Joone Hur <joone@outlook.com>
2026-08-31 16:44:25 -0700

    Use native Korean interface labels

--- fossbook.config.ko.js
+++ fossbook.config.ko.js
@@ -2,10 +2,14 @@ module.exports = {
   blogName: "F/OSS 코믹스",
   blogDescription:
     "F/OSS 코믹스는 자유 및 오픈 소스 소프트웨어를 다루는 만화 시리즈입니다. 컴퓨터와 소프트웨어의 역사를 통해 자유 및 오픈 소스 소프트웨어의 세계를 이해하고 직접 참여할 수 있도록 돕고자 합니다.",
-  homeLabel: "대문",
+  homeLabel: "처음",
   allPostsLabel: "모든 글",
-  aboutLabel: "소개",
+  aboutLabel: "이곳은",
   tagsLabel: "태그",
   allTagsLabel: "모든 태그",
-  transcriptLabel: "대화 텍스트 표시",
+  postedOnLabel: "올린 날:",
+  readMoreLabel: "더 읽기",
+  previousPageLabel: "앞으로",
+  nextPageLabel: "뒤로",
+  transcriptLabel: "말글 보이기",
 };
